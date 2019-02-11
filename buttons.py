f56a9f5aeeec721a6d1af6a209390e2d160f4543
Hugo <hlld@hotmail.fr>
2019-02-11 16:01:30 +0100

    finishing python script

--- buttons.py
+++ buttons.py
@@ -1,5 +1,6 @@
 from gpiozero import Button
 from time import sleep
+import pyautogui
 
 button3 = Button(19)
 button2 = Button(13)
@@ -7,12 +8,12 @@ button1 = Button(6)
 
 while True:
     if button1.is_pressed:
-        print("Pressed1")
-        sleep(1)
+	pyautogui.press('1')
+        sleep(0.6)
     if button2.is_pressed:
-        print("Pressed2")
-        sleep(1)
+	pyautogui.press('2')
+        sleep(0.6)
     if button3.is_pressed:
-        print("Pressed3")
-        sleep(1)
+	pyautogui.press('3')
+        sleep(0.6)
     sleep(0.1)
