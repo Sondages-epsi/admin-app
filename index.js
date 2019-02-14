199159b9ac6cfa3118a38977fb51113e2ba99197
Hugo <ledall.hugo.pro@gmail.com>
2019-02-14 12:00:10 +0100

    adding cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const env = require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
